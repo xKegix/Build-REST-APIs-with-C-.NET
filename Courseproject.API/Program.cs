b829e8ed10c4b1abe5ea0b4220ab1bae68c47857
keginold philip <xkegix@users.noreply.github.com>
2023-03-28 20:50:36 +0000

    Add project files.

---
+++ Courseproject.API/Program.cs
@@ -0,0 +1,45 @@
+using Courseproject.API;
+using Courseproject.Business;
+using Courseproject.Common.Interfaces;
+using Courseproject.Common.Model;
+using Courseproject.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+DIConfiguration.RegisterServices(builder.Services);
+builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddScoped<IGenericRepository<Address>, GenericRepository<Address>>();
+builder.Services.AddScoped<IGenericRepository<Job>, GenericRepository<Job>>();
+builder.Services.AddScoped<IGenericRepository<Employee>, GenericRepository<Employee>>();
+builder.Services.AddScoped<IGenericRepository<Team>, GenericRepository<Team>>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.EnsureCreated();
+}
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
